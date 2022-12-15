fa92ecd2e2db430d549d30c7de320041e8066234
Nikolai Gurov <gurov.nicolay@gmail.com>
2022-12-15 21:41:53 +0300

    step 5.0

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
-import greeting from '../src/cli.js';
-greeting();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+import readlineSync from 'readline-sync';
 
-const randomNumber = Math.floor(Math.random() * 100);
-console.log(randomNumber);
+const getRandomNumber = () => Math.floor(Math.random() * 100);
+// const randomNumber = Math.floor(Math.random() * 100);
+// console.log(randomNumber);
+const isEven = (number) => (number % 2 === 0);
+const check = (number) => {
+  const result = isEven(number) ? 'yes' : 'no';
+  return result;
+//   console.log(result);
+};
+// check(randomNumber);
+
+const brainEven = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}`);
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const number = getRandomNumber();
+    const correctNumber = check(number);
+    console.log(`Question: ${number}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer !== correctNumber) {
+      console.log(`"${userAnswer}" is wrong ;(. Correct answer was "${correctNumber}"`);
+      return console.log(`Let's try again, ${userName}`);
+    }
+    console.log('Correct!');
+  }
+  return console.log(`Congratulations, ${userName}`);
+};
+
+brainEven();
